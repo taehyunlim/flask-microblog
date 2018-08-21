59e08d886c7987dbdf858dcdadb1a96dc1c123cd
Tae Lim <taehyun.lim@gmail.com>
2018-08-21 07:34:33 -0700

    Email error logging. Not tested.

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
+import logging, os
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from logging.handlers import SMTPHandler, RotatingFileHandler
 
 application = Flask(__name__)
 application.config.from_object(Config)
@@ -12,3 +14,28 @@ login = LoginManager(application)
 login.login_view = 'login'
 
 from app import routes, models, errors
+
+# Add SMTPHandler instace to the logger object to log errors by email
+if not application.debug:
+    # Only enable email logger if the email server configs info exists
+    # TEST: (venv) python -m smtpd -n -c DebuggingServer localhost:8025
+    if application.config['MAIL_SERVER']:
+        auth = None
+        if application.config['MAIL_USERNAME'] or application.config['MAIL_PASSWORD']:
+            auth = (application.config['MAIL_USERNAME'], application.config['MAIL_PASSWORD'])
+        secure = None
+        if application.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost = (application.config['MAIL_SERVER'], application.config['MAIL_PORT']),
+            fromaddr = 'no-reply@' + application.config['MAIL_SERVER'],
+            toaddrs = application.config['ADMINS'],
+            subject = 'Microblog Failure',
+            credentials = auth,
+            secure = secure
+        )
+        mail_handler.setLevel(logging.ERROR)
+        application.logger.addHandler(mail_handler)
+
+    # Logging to a file
+    
