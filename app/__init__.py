9b63ccd3cc84dead82c7933553fd4e0e432e8991
Tae Lim <taehyun.lim@gmail.com>
2018-08-22 22:42:21 -0700

    Added file logger and fixed profile edit form bug.

--- app/__init__.py
+++ app/__init__.py
@@ -27,15 +27,20 @@ if not application.debug:
         if application.config['MAIL_USE_TLS']:
             secure = ()
         mail_handler = SMTPHandler(
-            mailhost = (application.config['MAIL_SERVER'], application.config['MAIL_PORT']),
-            fromaddr = 'no-reply@' + application.config['MAIL_SERVER'],
-            toaddrs = application.config['ADMINS'],
-            subject = 'Microblog Failure',
-            credentials = auth,
-            secure = secure
-        )
+            mailhost=(application.config['MAIL_SERVER'], application.config['MAIL_PORT']),
+            fromaddr='no-reply@' + application.config['MAIL_SERVER'],
+            toaddrs=application.config['ADMINS'], subject='Microblog Failure',
+            credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
         application.logger.addHandler(mail_handler)
 
     # Logging to a file
-    
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/Microblog', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s: %(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    application.logger.addHandler(file_handler)
+    # Logging categories: DEBUG, INFO, WARNING, ERROR and CRITICAL
+    application.logger.setLevel(logging.INFO)
+    application.logger.info('Microblog startup')
