d12d0753bfa55a079204254167cf7f85f6dabd0e
Tae Lim <taehyun.lim@gmail.com>
2018-08-23 21:54:55 -0700

    Follower and followed association (auxilary) table

--- app/models.py
+++ app/models.py
@@ -4,15 +4,32 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from hashlib import md5
 
+# Create an association table for followers
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     pw_hash = db.Column(db.String(128))
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     datetime_joined = db.Column(db.DateTime, default=datetime.utcnow)
     datetime_last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # Posts: One-to-Many relationship
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    # Followers: Many-to-Many relationship
+    followed = db.relationship(
+        'User', # Self-referential
+        secondary=followers, # Association table defined above
+        primaryjoin=(followers.c.follower_id == id), # How left-side entity is linked: User's `id` matches the `follower_id` field of the association table
+        secondaryjoin=(followers.c.followed_id == id), # How the right-side entity (follwed user) is linked.
+        backref=db.backref('followers', lazy='dynamic'), # How this relationship will be accessed from the right-side entity (followed)
+        lazy='dynamic' # `dynamic` sets up the query to not run until specifically requested
+    )
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -27,6 +44,23 @@ class User(UserMixin, db.Model):
         # Encode email string as bytes and then store the hexadecimal digest string
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.follwed_id == user.id).count() > 0
+
+    def followed_posts(self):
+        return Post.query.join(followers, (followers.c.followed_id == Post.user_id))
+            .filter(followers.c.follower_id == self.id or Post.user_id == self.id )
+            .order_by(Post.timestamp.desc())
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
