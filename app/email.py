2a2e0a015ef8b177ff14cb651e5601412c22ec38
Tae Lim <taehyun.lim@gmail.com>
2018-08-29 08:57:01 -0700

    Added async email method

--- app/email.py
+++ app/email.py
@@ -1,12 +1,19 @@
 from flask import render_template
 from flask_mail import Message
 from app import mail, application
+from threading import Thread # Async process module
+
+def send_asyn_email(appl, msg):
+    with appl.app_context():
+        mail.send(msg)
 
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    mail.send(msg)
+    # mail.send(msg)
+    # Instead of .send method, invoke a background thread to send email
+    Thread(target=send_asyn_email, args=(application, msg)).start()
 
 def send_pw_reset_email(user):
     token = user.get_reset_pw_token()
