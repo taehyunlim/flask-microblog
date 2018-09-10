92ffd034328ac9be300d53faf8ff89344959a51b
tae <tae@easypost.com>
2018-09-09 23:31:44 -0700

    Using dotenv to manage env variables

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import os
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     # Get secret_key from environment variable or use hardcoded key
